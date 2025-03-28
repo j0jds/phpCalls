3bcbbfca40f2e8a5e7b42831bd79466b6429e9c0
j0jds <mrlucassilva7342@gmail.com>
2025-03-28 03:00:13 -0300

    ✨ feat(templates): adds option to view calls

--- templates/main.php
+++ templates/main.php
@@ -1,5 +1,5 @@
 <?php
-    include('../server/protection.php');
+include('../server/protection.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,10 +15,12 @@
         if ($_SESSION['tipo'] == 'usuario') {
             echo '<h3>Precisa de ajuda? Abra um chamado:</h3>';
             echo '<p><a href="openCalls.php">Abrir Chamado</a></p>';
+            echo '<h3>Meus chamados:</h3>';
+            echo '<p><a href="./userCalls.php">Visualizar meus chamados</a></p>';
         }
         if ($_SESSION['tipo'] !== 'usuario') {
-            echo '<h3>Chamados disponíveis aqui:</h3>';
-            echo '<p><a href="./professionals.php">Chamados</a></p>';
+            echo '<h3>Chamados disponíveis:</h3>';
+            echo '<p><a href="./professionals.php">Ver Chamados</a></p>';
         }
     ?>
 
