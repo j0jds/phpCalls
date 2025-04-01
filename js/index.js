bd08675665ba019a34c1ac15d5c27c7becc5956e
j0jds <mrlucassilva7342@gmail.com>
2025-04-01 00:22:57 -0300

    🐞 fix(styles, js): remove fade-in css class and implement animation directly in javascript
    - The fade-in class in css generated an error that broke the display of elements.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+// Animação 
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.style.opacity = 0;
+    requestAnimationFrame(() => {
+        document.body.style.transition = 'opacity 1s ease-in-out';
+        document.body.style.opacity = 1;
+    });
+});
+
+// Tipo do Profissional
 function toggleTipoProfissional() {
     var tipoConta = document.getElementById("tipo_conta").value;
     var tipoProfissionalField = document.getElementById("tipo_profissional_field");
